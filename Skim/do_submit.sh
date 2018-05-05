5f3bb22aa309330bd29f1908dbce0ea39bec20fc
Mengyao Shi <mengyao.shi@cern.ch>
2018-05-05 23:51:27 +0200

    running right now

--- Skim/do_submit.sh
+++ Skim/do_submit.sh
@@ -1,40 +1,51 @@
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DY1
-sh Submit_DY1.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DY1_50
-sh Submit_DY1_50.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DY3
-sh Submit_DY3.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DY4
-sh Submit_DY4.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DYJets
-sh Submit_DYJets.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DYJets_50_amcatnlo
-sh Submit_DYJets_50_amcatnlo.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DYJets_50_madgraph
-sh Submit_DYJets_50_madgraph.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DY_amcatnlo
+chmod 777 Submit_DY_amcatnlo.sh
+sh Submit_DY_amcatnlo.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_DY
+chmod 777 Submit_DY.sh
+sh Submit_DY.sh
 mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_ST_tW_antitop
+chmod 777 Submit_ST_tW_antitop.sh
 sh Submit_ST_tW_antitop.sh
 mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_ST_tW_top
+chmod 777 Submit_ST_tW_top.sh
 sh Submit_ST_tW_top.sh
 mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_TT
+chmod 777 Submit_TT.sh
 sh Submit_TT.sh
 mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_W1
+chmod 777 Submit_W1.sh
 sh Submit_W1.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_W4_v1
+chmod 777 Submit_W4_v1.sh
+sh Submit_W4_v1.sh
 mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_W4_v2
+chmod 777 Submit_W4_v2.sh
 sh Submit_W4_v2.sh
 mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_W4_v6
+chmod 777 Submit_W4_v6.sh
 sh Submit_W4_v6.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_WJets
-sh Submit_WJets.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_WW
-sh Submit_WW.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_WZTo1L1Nu2Q
-sh Submit_WZTo1L1Nu2Q.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_WZTo1L3Nu
-sh Submit_WZTo1L3Nu.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_W
+chmod 777 Submit_W.sh
+sh Submit_W.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_WW1L1Nu2Q
+chmod 777 Submit_WW1L1Nu2Q.sh
+sh Submit_WW1L1Nu2Q.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_WZ1L1Nu2Q
+chmod 777 Submit_WZ1L1Nu2Q.sh
+sh Submit_WZ1L1Nu2Q.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_WZ1L3Nu
+chmod 777 Submit_WZ1L3Nu.sh
+sh Submit_WZ1L3Nu.sh
 mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_WZ
+chmod 777 Submit_WZ.sh
 sh Submit_WZ.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_ZZTo2L2Q
-sh Submit_ZZTo2L2Q.sh
-mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_ZZTo4L
-sh Submit_ZZTo4L.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_ZZ2L2Q
+chmod 777 Submit_ZZ2L2Q.sh
+sh Submit_ZZ2L2Q.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_ZZ4L
+chmod 777 Submit_ZZ4L.sh
+sh Submit_ZZ4L.sh
+mkdir -p /afs/cern.ch/work/m/mshi/public/outputTauID/Out_ZZ
+chmod 777 Submit_ZZ.sh
+sh Submit_ZZ.sh
