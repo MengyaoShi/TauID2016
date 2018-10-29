cf2abe7ae5857e985fb1d2d76a658aa11e11fa02
MengyaoShi <mshi@ucdavis.edu>
2018-10-29 01:37:03 +0100

    prepare QCD shape uncertainty datacard

---
+++ Analyze/Create_QCDOriginal.py
@@ -0,0 +1,117 @@
+if __name__ == "__main__":
+
+    import ROOT
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--scale', default="nominal", choices=['nominal', 'up', 'down', 'wup', 'wdown', 'shapeup','shapedown'], help="Which TES?")
+    parser.add_argument('--var', default="invMass", choices=['invMass', 'muPt', 'tauPt', 'muMt', 'tauMt', 'tauMass', 'met', 'muMetMt', 'tauMetMt', 'ZPt', 'muEta', 'tauEta', 'muTauDPhi', 'muTauDR', 'zeta', 'tauDecayMode'], help="Which TES?")
+    options = parser.parse_args()
+
+    postfix=""
+    if (options.scale=="up"):
+        postfix="_TESUp"
+    if (options.scale=="down"):
+        postfix="_TESDown"
+
+    postfixP=""
+    if (options.scale=="up"):
+        postfixP="_TESUp"
+    if (options.scale=="down"):
+        postfixP="_TESDown"
+    if (options.scale=="wup"):
+        postfixP="_WnormUp"
+    if (options.scale=="wdown"):
+        postfixP="_WnormDown"
+    if (options.scale=="shapeup"):
+        postfixP="_QCDShapeUp"
+    if (options.scale=="shapedown"):
+        postfixP="_QCDShapeDown"
+
+    postfixF=""
+    if (options.scale=="up"):
+        postfixF="_TESUp"
+    if (options.scale=="down"):
+        postfixF="_TESDown"
+    if (options.scale=="wup"):
+        postfixF="_WnormUp"
+    if (options.scale=="wdown"):
+        postfixF="_WnormDown"
+    if (options.scale=="shapeup"):
+        postfixF="_QCDShapeUp"
+    if (options.scale=="shapedown"):
+        postfixF="_QCDShapeDown"
+    facteur=1.0
+    if (options.scale=="wup"):
+        facteur=1.10
+    if (options.scale=="wdown"):
+        facteur=0.90
+
+    if options.scale=="up" or options.scale=="down":
+       fDYB=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/DYB.root","r")
+       fDYJ=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/DYJ.root","r")
+       fDYS=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/DYS.root","r")
+       fW=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/W.root","r")
+       fTT=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/TT.root","r")
+       fVV=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/VV.root","r")
+       fData=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/Data.root","r")
+    else:
+       fDYB=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/DYB.root","r")
+       fDYJ=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/DYJ.root","r")
+       fDYS=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/DYS.root","r")
+       fW=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/W.root","r")
+       fTT=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/TT.root","r")
+       fVV=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/VV.root","r")
+       fData=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/Data.root","r")
+    fout=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/QCD.root","recreate")
+    print fDYB
+    print fData
+    fratio=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal/testMergeTwo.root")
+
+    hRatio=fratio.Get("OS/SS Ratio in inverted Muon Iso region")
+
+    hSS_pass=fData.Get("passSS/data_obs")
+    hSS_pass.Add(fDYB.Get("passSS/DYB"+postfix),-1)
+    hSS_pass.Add(fDYJ.Get("passSS/DYJ"+postfix),-1)
+    hSS_pass.Add(fTT.Get("passSS/TT"+postfix),-1)
+    hSS_pass.Add(fW.Get("passSS/W"+postfix),-1*facteur)
+    hSS_pass.Add(fVV.Get("passSS/VV"+postfix),-1)
+    hSS_pass.Add(fDYS.Get("passSS/DYS"+postfix),-1)
+
+    hSS_fail=fData.Get("failSS/data_obs")
+    hSS_fail.Add(fDYB.Get("failSS/DYB"+postfix),-1)
+    hSS_fail.Add(fDYJ.Get("failSS/DYJ"+postfix),-1)
+    hSS_fail.Add(fTT.Get("failSS/TT"+postfix),-1)
+    hSS_fail.Add(fW.Get("failSS/W"+postfix),-1*facteur)
+    hSS_fail.Add(fVV.Get("failSS/VV"+postfix),-1)
+    hSS_fail.Add(fDYS.Get("failSS/DYS"+postfix),-1)
+
+    hSS_pass.SetName("QCD"+postfixP)
+    print postfixP
+    hSS_fail.SetName("QCD"+postfixF)
+
+    if postfixF=="shapeup":
+        for i in range(0, hSS_pass.GetSize()):
+            hSS_pass.SetBinContent(i, hRatio.GetBinContent(i)*hSS_pass.GetBinContent(i)+hRatio.GetBinError(i))
+            hSS_fail.SetBinContent(i, hRatio.GetBinContent(i)*hSS_fail.GetBinContent(i)+hRatio.GetBinError(i))
+    if postfixF=="shapedown":
+        for i in range(0, hSS_pass.GetSize()):
+            hSS_pass.SetBinContent(i, hRatio.GetBinContent(i)*hSS_pass.GetBinContent(i)-hRatio.GetBinError(i))
+            hSS_fail.SetBinContent(i, hRatio.GetBinContent(i)*hSS_fail.GetBinContent(i)-hRatio.GetBinError(i))
+
+    for i in range(0,hSS_fail.GetSize()-2):
+	if hSS_fail.GetBinContent(i)<0:
+	    hSS_fail.SetBinContent(i,0)
+
+    for i in range(0,hSS_pass.GetSize()):
+        if hSS_pass.GetBinContent(i)<0:
+            hSS_pass.SetBinContent(i,0)
+
+    fout.cd()
+    dirFailOS=fout.mkdir("failOS")
+    dirFailOS.cd()
+    hSS_fail.Write()
+    dirPassOS=fout.mkdir("passOS")
+    dirPassOS.cd()
+    hSS_pass.Write()
+
---
+++ Analyze/Scale_WOriginal.py
@@ -0,0 +1,85 @@
+if __name__ == "__main__":
+
+    import ROOT
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--scale', default="nominal", choices=['nominal', 'up', 'down'], help="Which TES?")
+    parser.add_argument('--anti', default="iso", choices=['iso', 'anti'], help="Which TES?")
+    parser.add_argument('--var', default="invMass", choices=['invMass', 'muPt', 'tauPt', 'muMt', 'tauMt', 'tauMass', 'met', 'muMetMt', 'tauMetMt', 'ZPt', 'muEta', 'tauEta', 'muTauDPhi', 'muTauDR', 'zeta', 'tauDecayMode'], help="Which TES?")
+    options = parser.parse_args()
+
+    anti=""
+    if (options.anti=="anti"):
+        anti="anti_"
+
+    postfix=""
+    if (options.scale=="up"):
+        postfix="_TESUp"
+    if (options.scale=="down"):
+        postfix="_TESDown"
+
+    fDYB=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/DYB.root","r")
+    fDYJ=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/DYJ.root","r")
+    fDYS=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/DYS.root","r")
+    fW=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/Wunscaled.root","r")
+    fTT=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/TT.root","r")
+    fVV=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+options.scale+options.var+"/VV.root","r")
+    fData=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_nominal"+options.var+"/Data.root","r")
+    fout=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+anti+options.scale+options.var+"/W.root","recreate")
+
+    fW_=ROOT.TFile("/afs/cern.ch/work/m/mshi/public/files_"+anti+options.scale+options.var+"/Wunscaled.root","r")
+    print options.scale
+    print fData.Get("n70").GetBinContent(3)
+    print fVV.Get("n70").GetBinContent(3)
+    print fVV.Get("n70").GetBinContent(2)
+    print fVV.Get("n70").GetBinContent(1)
+    print fDYB.Get("n70").GetBinContent(3)
+    print fDYS.Get("n70").GetBinContent(3)
+    print fTT.Get("n70").GetBinContent(3)
+    print fW.Get("n70").GetBinContent(3)
+    QCDpassSS=fData.Get("n70").GetBinContent(3)-fVV.Get("n70").GetBinContent(3)-fDYJ.Get("n70").GetBinContent(3)-fDYB.Get("n70").GetBinContent(3)-fDYS.Get("n70").GetBinContent(3)-fTT.Get("n70").GetBinContent(3)-fW.Get("n70").GetBinContent(3)
+    QCDfailSS=fData.Get("n70").GetBinContent(4)-fVV.Get("n70").GetBinContent(4)-fDYJ.Get("n70").GetBinContent(4)-fDYB.Get("n70").GetBinContent(4)-fDYS.Get("n70").GetBinContent(4)-fTT.Get("n70").GetBinContent(4)-fW.Get("n70").GetBinContent(4)
+    QCDall=fData.Get("n70").GetBinContent(3)+fData.Get("n70").GetBinContent(4)-fVV.Get("n70").GetBinContent(3)-fVV.Get("n70").GetBinContent(4)-fDYJ.Get("n70").GetBinContent(3)-fDYJ.Get("n70").GetBinContent(4)-fDYB.Get("n70").GetBinContent(3)-fDYB.Get("n70").GetBinContent(4)-fDYS.Get("n70").GetBinContent(3)-fDYS.Get("n70").GetBinContent(4)-fTT.Get("n70").GetBinContent(3)-fTT.Get("n70").GetBinContent(4)-fW.Get("n70").GetBinContent(3)-fW.Get("n70").GetBinContent(4)
+    if QCDpassSS<0:
+	QCDpassSS=0
+    if QCDfailSS<0:
+	QCDfailSS=0
+    if QCDall<0:
+        QCDall=0
+    #QCDpassSS=0
+    #QCDfailSS=0
+    print "nb events: ",fData.Get("n70").GetBinContent(1)
+    print QCDpassSS,QCDfailSS
+    SFpassOS=(fData.Get("n70").GetBinContent(1)-fVV.Get("n70").GetBinContent(1)-fDYJ.Get("n70").GetBinContent(1)-fDYB.Get("n70").GetBinContent(1)-fDYS.Get("n70").GetBinContent(1)-fTT.Get("n70").GetBinContent(1)-1.05*QCDpassSS)/(fW.Get("n70").GetBinContent(1))
+    print "this is debugging code, and SFpassOS, SFfailOS, SFall="
+    SFfailOS=(fData.Get("n70").GetBinContent(2)-fVV.Get("n70").GetBinContent(2)-fDYJ.Get("n70").GetBinContent(2)-fDYB.Get("n70").GetBinContent(2)-fDYS.Get("n70").GetBinContent(2)-fTT.Get("n70").GetBinContent(2)-1.05*QCDfailSS)/fW.Get("n70").GetBinContent(2)
+    SFall=(fData.Get("n70").GetBinContent(1)+fData.Get("n70").GetBinContent(2)-fVV.Get("n70").GetBinContent(1)-fVV.Get("n70").GetBinContent(2)-fDYJ.Get("n70").GetBinContent(1)-fDYJ.Get("n70").GetBinContent(2)-fDYB.Get("n70").GetBinContent(1)-fDYB.Get("n70").GetBinContent(2)-fDYS.Get("n70").GetBinContent(1)-fDYS.Get("n70").GetBinContent(2)-fTT.Get("n70").GetBinContent(1)-fTT.Get("n70").GetBinContent(2)-1.05*QCDall)/(fW.Get("n70").GetBinContent(1)+fW.Get("n70").GetBinContent(2))
+    print SFpassOS, SFfailOS,SFall
+    hWpassOS=fW_.Get("passOS/W"+postfix)
+    hWpassOS.Scale(SFall)
+    hWfailOS=fW_.Get("failOS/W"+postfix)
+    hWfailOS.Scale(SFall)
+    hWpassSS=fW_.Get("passSS/W"+postfix)
+    hWpassSS.Scale(SFall)
+    hWfailSS=fW_.Get("failSS/W"+postfix)
+    hWfailSS.Scale(SFall)
+
+    fout.cd()
+    dirPassOS=fout.mkdir("passOS")
+    dirPassOS.cd()
+    hWpassOS.SetName("W"+postfix)
+    hWpassOS.Write()
+    dirFailOS=fout.mkdir("failOS")
+    dirFailOS.cd()
+    hWfailOS.SetName("W"+postfix)
+    hWfailOS.Write()
+    dirPassSS=fout.mkdir("passSS")
+    dirPassSS.cd()
+    hWpassSS.SetName("W"+postfix)
+    hWpassSS.Write()
+    dirFailSS=fout.mkdir("failSS")
+    dirFailSS.cd()
+    hWfailSS.SetName("W"+postfix)
+    hWfailSS.Write()
+
